7f095e30a0245eed7d3e2a5edf7f048e024af8e4
Nishkarsh Sharma <nishkarsh.sharma@sezzle.com>
2022-08-14 18:54:22 +0200

    Fix the formatting of output CSV to include spaces between elements in JSON array.

--- src/csv-formatter.ts
+++ src/csv-formatter.ts
@@ -5,7 +5,12 @@ import { columns } from './types/output-row';
 const defaultFormatterOptions = {
     header: true,
     quoted_match: /^\[.*]$/,
-    columns: columns
+    columns: columns,
+    cast: {
+        object: (value: object): string => {
+            return JSON.stringify(value).replace(/,/g, ", ");
+        }
+    }
 }
 
 export const createCsvFormatter = (options: Options = defaultFormatterOptions): Transform => {
